d48e4c656d3bdf11d9b36a9948ab1eccbb3629b9
wx_shadow <wx_shadow@DESKTOP-8FKIGML>
2018-01-17 20:02:37 +0800

    v0.11 角色连接完成

--- testfolder/com/dream/test/folder/UserTest.java
+++ testfolder/com/dream/test/folder/UserTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.dream.pojo.Role;
 import com.dream.pojo.User;
 import com.dream.service.impl.UserService;
 
@@ -19,12 +20,27 @@ public class UserTest {
 	/**
 	 * 测试根据uid查询进行登陆
 	 */
+//	@Test
+//	public void queryuser() {
+//		User user=new User();
+//		user.setYmId("ym001");
+//		UserService userService=(UserService) ac.getBean("userservice");
+//		user=userService.queryuser(user);
+//		System.out.println(user.getuName()+"\t"+user.getuPhone()+"\t"+user.getuSex()  );
+//	}
+//	
+//	@Test
+//	public void getRole() {
+//		
+//		UserService userService=(UserService) ac.getBean("userservice");
+//		userService.getRoleById(1);
+//		
+//	}
+	
 	@Test
-	public void queryuser() {
-		User user=new User();
-		user.setYmId("ym001");
+	public void defineUserToRole(){
 		UserService userService=(UserService) ac.getBean("userservice");
-		user=userService.queryuser(user);
-		System.out.println(user.getuName()+"\t"+user.getuPhone()+"\t"+user.getuSex()  );
+		userService.defineRoleToUser(1, 1);
 	}
+	
 }
